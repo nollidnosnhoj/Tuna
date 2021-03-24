cbf5d18d31dd441918ac455f5d0ccf07c5f94488
Dillon Johnson <nollidnosnhoj@gmail.com>
2021-03-24 00:45:00 -1000

    homepage

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import useUser from "~/hooks/useUser";
 import Page from "~/components/Page";
+import { HomepageCTA } from "~/components/CTA";
 
 const Index = () => {
-  return <Page></Page>;
+  return (
+    <Page>
+      <HomepageCTA />
+    </Page>
+  );
 };
 
 export default Index;
