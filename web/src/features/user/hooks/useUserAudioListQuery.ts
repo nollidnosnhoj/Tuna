e4ea923b0638de3602aba52f47980e68ea06e0b9
Dillon Johnson <nollidnosnhoj@gmail.com>
2021-05-19 12:33:02 -1000

    Update useUserAudioListQuery.ts

--- web/src/features/user/hooks/useUserAudioListQuery.ts
+++ web/src/features/user/hooks/useUserAudioListQuery.ts
@@ -1,15 +1,23 @@
 import { useRouter } from "next/router";
+import { UseInfiniteQueryOptions } from "react-query";
 import { Audio } from "~/features/audio/types";
 import { fetch } from "~/lib/api";
 import { useAuth } from "~/lib/hooks/useAuth";
 import useInfiniteCursorPagination, {
   UseInfiniteCursorPaginationReturnType,
 } from "~/lib/hooks/useInfiniteCursorPagination";
+import { CursorPagedList } from "~/lib/types";
 
 export function useUserAudioListQuery(
-  username: string
+  username: string,
+  size?: number,
+  queryOptions?: UseInfiniteQueryOptions<CursorPagedList<Audio>>
 ): UseInfiniteCursorPaginationReturnType<Audio> {
   const { query } = useRouter();
+  const params = { ...query };
+  if (typeof size !== "undefined" && size > 0) {
+    Object.assign(params, { size });
+  }
   const { accessToken } = useAuth();
   return useInfiniteCursorPagination<Audio>(
     `users/${username}/audios`,
@@ -17,10 +25,11 @@ export function useUserAudioListQuery(
       if (!cursor) cursor = undefined;
       return fetch(
         `users/${username}/audios`,
-        { ...query, cursor: cursor },
+        { ...params, cursor: cursor },
         { accessToken }
       );
     },
-    query
+    query,
+    queryOptions
   );
 }
