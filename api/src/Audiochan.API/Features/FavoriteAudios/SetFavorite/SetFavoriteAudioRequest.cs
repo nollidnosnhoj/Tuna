bf9ea85fb8c8117edb0535e1556507c0b08ebfa0
Dillon Johnson <nollidnosnhoj@gmail.com>
2021-06-01 12:57:56 -1000

    set favorite

---
+++ api/src/Audiochan.API/Features/FavoriteAudios/SetFavorite/SetFavoriteAudioRequest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Audiochan.Core.Entities;
+using Audiochan.Core.Models;
+using Audiochan.Core.Services;
+using MediatR;
+
+namespace Audiochan.API.Features.FavoriteAudios.SetFavorite
+{
+    public record SetFavoriteAudioRequest(Guid AudioId, string UserId, bool IsFavoriting) : IRequest<Result<bool>>
+    {
+    }
+    
+    public class SetFavoriteAudioRequestHandler : IRequestHandler<SetFavoriteAudioRequest, Result<bool>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IDateTimeProvider _dateTimeProvider;
+
+        public SetFavoriteAudioRequestHandler(IUnitOfWork unitOfWork, IDateTimeProvider dateTimeProvider)
+        {
+            _unitOfWork = unitOfWork;
+            _dateTimeProvider = dateTimeProvider;
+        }
+
+        public async Task<Result<bool>> Handle(SetFavoriteAudioRequest request, CancellationToken cancellationToken)
+        {
+            var audio = await _unitOfWork.Audios.GetAsync(
+                new GetTargetAudioForFavoritingSpecification(request.AudioId), true, cancellationToken);
+            
+            if (audio == null)
+                return Result<bool>.Fail(ResultError.NotFound);
+
+            var isFavoriting = request.IsFavoriting
+                ? await Favorite(audio, request.UserId, cancellationToken)
+                : await Unfavorite(audio, request.UserId, cancellationToken);
+
+            _unitOfWork.Audios.Update(audio);
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+            return Result<bool>.Success(isFavoriting);
+        }
+        
+        private Task<bool> Favorite(Audio target, string userId, CancellationToken cancellationToken = default)
+        {
+            var favoriter = target.Favorited.FirstOrDefault(f => f.UserId == userId);
+
+            if (favoriter is null)
+            {
+                favoriter = new FavoriteAudio
+                {
+                    AudioId = target.Id,
+                    UserId = userId,
+                    FavoriteDate = _dateTimeProvider.Now
+                };
+                
+                target.Favorited.Add(favoriter);
+            }
+            else if (favoriter.UnfavoriteDate is not null)
+            {
+                favoriter.FavoriteDate = _dateTimeProvider.Now;
+                favoriter.UnfavoriteDate = null;
+            }
+            
+            return Task.FromResult(true);
+        }
+
+        private Task<bool> Unfavorite(Audio target, string userId, CancellationToken cancellationToken = default)
+        {
+            var favoriter = target.Favorited.FirstOrDefault(f => f.UserId == userId);
+
+            if (favoriter is not null)
+            {
+                favoriter.UnfavoriteDate = _dateTimeProvider.Now;
+            }
+
+            return Task.FromResult(false);
+        }
+    }
+}
---
+++ api/tests/Audiochan.Core.IntegrationTests/Features/FavoriteAudios/SetFavoriteAudioTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using Audiochan.API.Features.FavoriteAudios.SetFavorite;
+using Audiochan.Tests.Common.Fakers.Audios;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace Audiochan.Core.IntegrationTests.Features.FavoriteAudios
+{
+    [Collection(nameof(SliceFixture))]
+    public class SetFavoriteAudioTests
+    {
+        private readonly SliceFixture _sliceFixture;
+
+        public SetFavoriteAudioTests(SliceFixture sliceFixture)
+        {
+            _sliceFixture = sliceFixture;
+        }
+        
+        [Fact]
+        public async Task AddFavoriteTest()
+        {
+            var (targetId, _) = await _sliceFixture.RunAsDefaultUserAsync();
+            
+            var (observerId, _) =
+                await _sliceFixture.RunAsUserAsync("kopacetic", "kopacetic123!", Array.Empty<string>());
+
+            var audio = new AudioFaker(targetId).Generate();
+            await _sliceFixture.InsertAsync(audio);
+
+            await _sliceFixture.SendAsync(new SetFavoriteAudioRequest(audio.Id, observerId, true));
+
+            var refetchAudio = await _sliceFixture.ExecuteDbContextAsync(database =>
+            {
+                return database.Audios
+                    .AsNoTracking()
+                    .Include(u => u.Favorited)
+                    .SingleOrDefaultAsync(a => a.Id == audio.Id);
+            });
+
+            refetchAudio.Favorited.Should().NotBeEmpty();
+            refetchAudio.Favorited.Should().Contain(x => x.UserId == observerId && x.AudioId == audio.Id);
+        }
+    }
+}
