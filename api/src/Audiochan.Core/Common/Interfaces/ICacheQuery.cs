3e14aa7fbe55060d5326d3b0d489cf497ed4f58a
Dillon Johnson <nollidnosnhoj@gmail.com>
2021-06-07 14:06:35 -1000

    cache query pipeline

---
+++ api/src/Audiochan.Core/Common/Interfaces/ICacheQuery.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Audiochan.Core.Common.Interfaces
+{
+    public interface ICacheQuery
+    {
+        string CacheKey { get; }
+        bool BypassCache { get; }
+        TimeSpan? CacheExpiration { get; }
+    }
+}
