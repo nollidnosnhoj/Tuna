c53c56ecac838f707062dc97f2c043087e05abff
Dillon Johnson <nollidnosnhoj@gmail.com>
2021-06-23 10:25:26 -1000

    .

--- web/src/utils/http.spec.ts
+++ web/src/utils/http.spec.ts
@@ -1,4 +1,4 @@
-import { stringifyQueryObject } from "./http";
+import { objectToFormData, stringifyQueryObject } from "./http";
 
 describe("stringifyQueryObject", () => {
   it("Successfully stringify object", () => {
@@ -10,5 +10,19 @@ describe("stringifyQueryObject", () => {
     const expected = "?paramOne=hello&paramTwo=true&paramThree=10";
     const result = stringifyQueryObject(mock);
     expect(result).toBe(expected);
+  });
+});
+
+describe("objectToFormData", () => {
+  it("Successfully transform JS Object into FormData", () => {
+    const obj = {
+      test1: "hello",
+      test2: "world",
+    };
+    const fd = objectToFormData(obj);
+    expect(fd.has("test1")).toBeTruthy();
+    expect(fd.has("test2")).toBeTruthy();
+    expect(fd.get("test1")).toBe("hello");
+    expect(fd.get("test2")).toBe("world");
   });
 });
