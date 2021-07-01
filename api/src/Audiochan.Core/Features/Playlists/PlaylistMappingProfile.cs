f5a9a001726582cfbffcf4accff1b876690de769
Dillon Johnson <nollidnosnhoj@gmail.com>
2021-06-30 19:32:03 -1000

    Create PlaylistMappingProfile.cs

---
+++ api/src/Audiochan.Core/Features/Playlists/PlaylistMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using Audiochan.Core.Entities;
+using Audiochan.Core.Features.Playlists.GetPlaylist;
+using AutoMapper;
+
+namespace Audiochan.Core.Features.Playlists
+{
+    public class PlaylistMappingProfile : Profile
+    {
+        public PlaylistMappingProfile()
+        {
+            CreateMap<Playlist, PlaylistDetailViewModel>();
+        }
+    }
+}
