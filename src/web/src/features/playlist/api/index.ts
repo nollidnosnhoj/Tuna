3fd1531a784e25980582d81ce374440222131cfb
Dillon Johnson <nollidnosnhoj@gmail.com>
2021-08-18 14:16:41 -1000

    im silly... removed a method on accident

--- src/web/src/features/playlist/api/index.ts
+++ src/web/src/features/playlist/api/index.ts
@@ -1,19 +1,16 @@
+import { AudioId } from "~/features/audio/api/types";
 import request from "~/lib/http";
 
-export async function checkIfPlaylistFavoritedRequest(
-  playlistId: string
-): Promise<boolean> {
-  try {
-    const res = await request({
-      method: "head",
-      url: `me/favorites/playlists/${playlistId}`,
-      validateStatus: (status) => {
-        return status === 404 || status < 400;
-      },
-    });
-
-    return res.status !== 404;
-  } catch (err) {
-    return false;
-  }
+export async function checkDuplicatedAudiosRequest(
+  id: number,
+  audioIds: AudioId[]
+): Promise<AudioId[]> {
+  const { data } = await request<AudioId[]>({
+    method: "post",
+    url: `playlists/${id}/audios/duplicate`,
+    data: {
+      audioIds,
+    },
+  });
+  return data;
 }
