5f1e7de748ec57cc67b02841e79b89ef2eb6d3fe
Dillon Johnson <nollidnosnhoj@gmail.com>
2021-12-26 17:25:06 -1000

    redo faker
    cant use rulefor for records with positional parameters.

--- src/api/tests/Audiochan.Tests.Common/Fakers/Audios/UpdateAudioCommandFaker.cs
+++ src/api/tests/Audiochan.Tests.Common/Fakers/Audios/UpdateAudioCommandFaker.cs
@@ -1,4 +1,5 @@
-﻿using Audiochan.Application.Features.Audios.Commands.UpdateAudio;
+﻿using System.Linq;
+using Audiochan.Application.Features.Audios.Commands.UpdateAudio;
 using Bogus;
 
 namespace Audiochan.Tests.Common.Fakers.Audios
@@ -7,11 +8,11 @@ namespace Audiochan.Tests.Common.Fakers.Audios
     {
         public UpdateAudioCommandFaker(long audioId)
         {
-            RuleFor(x => x.AudioId, audioId);
-            RuleFor(x => x.Title, f => f.Random.String2(3, 30));
-            RuleFor(x => x.Description, f => f.Lorem.Sentences(2));
-            RuleFor(x => x.Tags, f => 
-                f.Make<string>(5, _ => f.Random.String2(5, 10)));
+            CustomInstantiator(f =>
+                new UpdateAudioCommand(audioId, 
+                    f.Random.String2(3, 30), 
+                    f.Lorem.Sentences(2),
+                    f.Make<string>(5, _ => f.Random.String2(5, 10)).ToArray()));
         }
     }
 }
