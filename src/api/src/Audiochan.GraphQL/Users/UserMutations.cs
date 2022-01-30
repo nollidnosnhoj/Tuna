785801f5858d812d750b128ea399f5a437849161
Dillon Johnson <nollidnosnhoj@gmail.com>
2022-01-30 11:23:06 -1000

    Revert "combine email and username update to user update"
    This reverts commit e60d1dfec1d2ee6be045825c70b1048b67dd5298.

--- src/api/src/Audiochan.GraphQL/Users/UserMutations.cs
+++ src/api/src/Audiochan.GraphQL/Users/UserMutations.cs
@@ -3,10 +3,11 @@ using Audiochan.Application.Commons.Dtos.Responses;
 using Audiochan.Application.Commons.Extensions;
 using Audiochan.Application.Features.Users.Commands.RemovePicture;
 using Audiochan.Application.Features.Users.Commands.SetFollow;
+using Audiochan.Application.Features.Users.Commands.UpdateEmail;
 using Audiochan.Application.Features.Users.Commands.UpdatePassword;
 using Audiochan.Application.Features.Users.Commands.UpdatePicture;
 using Audiochan.Application.Features.Users.Commands.UpdateProfile;
-using Audiochan.Application.Features.Users.Commands.UpdateUser;
+using Audiochan.Application.Features.Users.Commands.UpdateUsername;
 using Audiochan.Application.Features.Users.Models;
 using Audiochan.Domain.Entities;
 using Audiochan.GraphQL.Common.Errors;
@@ -63,25 +64,41 @@ public class UserMutations
         await mediator.Send(command, cancellationToken);
         return true;
     }
-    
-    [UseMutationConvention]
+
+    [UseMutationConvention(PayloadFieldName = "success")]
     [Authorize]
     [Error(typeof(ValidationError))]
     [Error(typeof(Forbidden))]
     [Error(typeof(UsernameTaken))]
-    [Error(typeof(EmailTaken))]
-    public async Task<UserDto> UpdateUser(
-        string? username,
-        string? email,
+    public async Task<bool> UpdateUsername(
+        string username,
         ClaimsPrincipal claimsPrincipal,
         [Service] IMediator mediator,
         CancellationToken cancellationToken = default)
     {
         var userId = claimsPrincipal.GetUserId();
-        var command = new UpdateUserCommand(userId, username, email);
-        return await mediator.Send(command, cancellationToken);
+        var command = new UpdateUsernameCommand(userId, username);
+        await mediator.Send(command, cancellationToken);
+        return true;
     }
-
+    
+    [UseMutationConvention(PayloadFieldName = "success")]
+    [Authorize]
+    [Error(typeof(ValidationError))]
+    [Error(typeof(Forbidden))]
+    [Error(typeof(EmailTaken))]
+    public async Task<bool> UpdateEmail(
+        string email,
+        ClaimsPrincipal claimsPrincipal,
+        [Service] IMediator mediator,
+        CancellationToken cancellationToken = default)
+    {
+        var userId = claimsPrincipal.GetUserId();
+        var command = new UpdateEmailCommand(userId, email);
+        await mediator.Send(command, cancellationToken);
+        return true;
+    }
+    
     [UseMutationConvention(PayloadFieldName = "success")]
     [Authorize]
     [Error(typeof(ValidationError))]
---
+++ src/api/tests/Audiochan.Application.UnitTests/Validations/Users/UpdateEmailCommandValidatorTests.cs
@@ -0,0 +1,47 @@
+﻿using Audiochan.Application.Features.Users.Commands.UpdateEmail;
+using FluentAssertions;
+using FluentValidation;
+using FluentValidation.TestHelper;
+using Xunit;
+
+namespace Audiochan.Application.UnitTests.Validations.Users
+{
+    public class UpdateEmailCommandValidatorTests
+    {
+        private readonly IValidator<UpdateEmailCommand> _validator;
+
+        public UpdateEmailCommandValidatorTests()
+        {
+            _validator = new UpdateEmailCommandValidator();
+        }
+
+        [Theory]
+        [InlineData("applesauce@example.com")]
+        [InlineData("example@example.com")]
+        [InlineData("testuser123@example.com")]
+        public void ShouldBeValid(string email)
+        {
+            var request = new UpdateEmailCommand(1, email);
+            var testResults = _validator.TestValidate(request);
+            testResults.IsValid.Should().BeTrue();
+        }
+        
+        [Fact]
+        public void ShouldNotBeValid_WhenEmailIsMissing()
+        {
+            var request = new UpdateEmailCommand(1, "");
+            var testResults = _validator.TestValidate(request);
+            testResults.ShouldHaveValidationErrorFor(x => x.NewEmail);
+        }
+        
+        [Theory]
+        [InlineData("testuser123")]
+        [InlineData("example123")]
+        public void ShouldNotBeValid_WhenEmailIsInvalid(string email)
+        {
+            var request = new UpdateEmailCommand(1, email);
+            var testResults = _validator.TestValidate(request);
+            testResults.ShouldHaveValidationErrorFor(x => x.NewEmail);
+        }
+    }
+}
