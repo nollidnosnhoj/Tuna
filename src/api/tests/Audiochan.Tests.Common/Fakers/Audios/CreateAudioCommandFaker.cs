e6e508cc1c8c47a9f73761eb38c8f83e615dc2a6
Dillon Johnson <nollidnosnhoj@gmail.com>
2022-01-30 11:23:27 -1000

    Revert "redo faker"
    This reverts commit 5f1e7de748ec57cc67b02841e79b89ef2eb6d3fe.

--- src/api/tests/Audiochan.Tests.Common/Fakers/Audios/CreateAudioCommandFaker.cs
+++ src/api/tests/Audiochan.Tests.Common/Fakers/Audios/CreateAudioCommandFaker.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using Audiochan.Application.Features.Audios.Commands.CreateAudio;
 using Bogus;
 
@@ -9,15 +8,14 @@ namespace Audiochan.Tests.Common.Fakers.Audios
     {
         public CreateAudioCommandFaker()
         {
-            CustomInstantiator(faker => 
-                new CreateAudioCommand(
-                    Guid.NewGuid().ToString("N") + ".mp3",
-                    faker.Random.String2(3, 30),
-                    faker.Lorem.Sentences(2),
-                    faker.Make<string>(5, _ => faker.Random.String2(5, 10)).ToArray(),
-                    faker.System.FileName("mp3"),
-                    faker.Random.Number(1, 20_000_000),
-                    faker.Random.Number(1, 30)));
+            RuleFor(x => x.UploadId, Guid.NewGuid().ToString("N") + ".mp3");
+            RuleFor(x => x.FileName, f => f.System.FileName("mp3"));
+            RuleFor(x => x.FileSize, f => f.Random.Number(1, 20_000_000));
+            RuleFor(x => x.Duration, f => f.Random.Number(1, 300));
+            RuleFor(x => x.Title, f => f.Random.String2(3, 30));
+            RuleFor(x => x.Description, f => f.Lorem.Sentences(2));
+            RuleFor(x => x.Tags, f => 
+                f.Make<string>(5, _ => f.Random.String2(5, 10)));
         }
     }
 }
