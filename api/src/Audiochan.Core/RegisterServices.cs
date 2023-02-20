1b234392da84111617ad1827df694b4b7ef980c0
Dillon Johnson <nollidnosnhoj@gmail.com>
2023-02-20 12:51:10 -1000

    remove deprecated package

--- api/src/Audiochan.Core/RegisterServices.cs
+++ api/src/Audiochan.Core/RegisterServices.cs
@@ -17,7 +17,7 @@ public static class RegisterServices
 {
     public static IServiceCollection AddApplication(this IServiceCollection services, IConfiguration configuration, IHostEnvironment environment)
     {
-        services.AddMediatR(Assembly.GetExecutingAssembly());
+        services.AddMediatR(config => config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
         services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
         services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationPipelineBehavior<,>));
         services.AddTransient(typeof(IPipelineBehavior<,>), typeof(DbContextTransactionPipelineBehavior<,>));
